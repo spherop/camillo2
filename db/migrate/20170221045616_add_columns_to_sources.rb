82a2b1f283541fc11c9878984333cef8a6ae4c91
Dan Newman <dpnewman at gmail>
2017-02-21 10:47:59 -0800

    fixed migration

--- db/migrate/20170221045616_add_columns_to_sources.rb
+++ db/migrate/20170221045616_add_columns_to_sources.rb
@@ -1,5 +1,6 @@
 class AddColumnsToSources < ActiveRecord::Migration[5.0]
   def change
+    rename_table :books, :sources
     add_column :sources, :source_type, :string, :default => ""
     add_column :sources, :time, :string, :default => "00:00"
   end
