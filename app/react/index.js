85cb37aa2af1bdca072e35739508c5f31eae30b8
Dan Newman <dpnewman at gmail>
2017-03-05 11:09:29 -0800

    cleanup

--- app/react/index.js
+++ app/react/index.js
@@ -1,7 +1,5 @@
 import RWR from 'react-webpack-rails';
 RWR.run();
 
-// import NavButtons from './components/nav/nav-buttons';
 import App from './components/app';
-// RWR.registerComponent('NavButtons', NavButtons);
 // RWR.registerComponent('App', App);
